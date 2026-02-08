ab58de081979adc7470b0be00a1d3a983ea64a4c
paologua <nex.dpg@gmail.com>
2026-02-08 18:39:50 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+CHROMA_PATH = "hive_memory"
